8952b0d078b28f8b69bdef03b59b664175d52802
Dion Singh <dionsingh8@hotmail.com>
2021-10-05 23:06:15 -0400

    Learnt about the "this" keyword

--- OOP/this.js
+++ OOP/this.js
@@ -1,19 +1,29 @@
 //-----[THIS KEYWORD]---------------------------------------------------------------
 
-// IT REPRESENTS AN OBJECT 
+// BY DEFAULT IT REPRESENTS AN OBJECT 
 
 // THE OBJECT VARIES DEPENDING ON THE CONTEXT
 
-// SOME EXAMPLES OF THESE CONTEXTS:
-// - ADDING EVENTS TO HTML ELEMENTS
-// - USING THE BIND() FUNCTION
-// - ARROW FUNCTIONS
+function dbz() {console.log(this)} 
+dbz(); // THE GLOBAL WINDOW OBJECT WILL BE PRINTED TO THE CONSOLE
 
 
+//--[ADDING EVENT LISTENERS TO HTML DOM ELEMENTS]------------------------------------
 
+// TARGETING AN ELEMENT IN THE DOM
+const title = document.getElementById("title");
 
+// REGULAR FUNCTIONS
+// "THIS" WILL REFER TO THE DOM ELEMENT
+title.addEventListener("click", function() {
+    console.log(this);
+})
 
-
+// ARROW FUNCTIONS
+// "THIS" WILL REFER TO THE GLOBAL ENVIRONMENT
+title.addEventListener("click", () => {
+    console.log(this);
+})
 
 
 
