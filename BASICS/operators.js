eca9d9323d25bc4959213babde2a5f70b6789cd0
Dion Singh <dionsingh8@hotmail.com>
2021-10-06 23:02:06 -0400

    More practice with the Ternary Operator "?"

--- BASICS/operators.js
+++ BASICS/operators.js
@@ -20,16 +20,22 @@ console.log((age >= 18) ? "Adult" : "Underage");
 
 //-----[EXAMPLE 3]------------------------------------------------------------------------------------------------
 
+// ADDING MULTIPLE EXPRESSIONS TO BE EXECUTED 
+// IF THE CONDITION IS TRUE OR FALSE
+
 var stop; // THIS WILL TAKE A BOOLEAN VALUE
 
-(age >= 18) ? (
-    consol.log("ADULT!!!"),
+// STOP WILL BE FALSE IF THE AGE IS ABOVE 18 
+// AND TRUE IF IT IS BELOW 18 SO THAT UNDERAGE 
+// PERSONS ARE STOPPED
+
+age >= 18 ? (
+    console.log("ADULT!!!"),
     stop = false
 ) : (
     console.log("SORRY, TOO YOUNG"),
     stop = true
 );
-
 
 //-----[EXAMPLE 4]----------------------------------------------------------------
 
@@ -41,5 +47,26 @@ let result = (mark > 50) ? "Pass" : "Fail";
 
 console.log(result);
 
+
+//-----[EXAMPLE 5]----------------------------------------------------------------
+
+// EMBEDDING A TERNARY OPERATOR WITHIN ANOTHER 
+// TERNARY OPERATOR 
+
+// CREATING 2 BOOLEAN VARIABLES
+let firstCheck = false;
+let secondCheck = false;
+
+// CREATING THE TERNARY OPERATOR
+let access = firstCheck ? "Access Denied" : secondCheck ? "Access Denied" : "Access Granted";
+
+
+//-----[EXAMPLE 6]-------------------------------------------------------------------------------------------------------
+
+// CHECKING IF X IS WITHIN A RANGE OF 10-20
+
+let x = 15;
+
+(x < 10) ? "Outside Range" : (x > 20) ? "Outside Range" : "Inside Range";
 
 
