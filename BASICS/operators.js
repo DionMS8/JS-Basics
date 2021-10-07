1f52b571694494bf24f9e68cf4af787f3fca261c
Dion Singh <dionsingh8@hotmail.com>
2021-10-06 22:32:55 -0400

    Practiced using the Ternary Conditional Operator ?

--- BASICS/operators.js
+++ BASICS/operators.js
@@ -2,6 +2,8 @@
 
 //-----[TERNARY/CONDITIONAL/QUESTION MARK OPERATOR]---------------------------------------------------------------
 
+// THIS IS A SHORTCUT FOR AN IF STATEMENT
+
 // SYNTAX: CONDITION ? VALUE IF TRUE : VALUE IF FALSE
 
 //-----[EXAMPLE 1]-------------------------------------------------------------------------------------------------
@@ -11,12 +13,33 @@ let output = (10 > 0) ? true : false; // => TRUE
 
 //-----[EXAMPLE 2]--------------------------------------------------------------------------------------------------
 
+var age = 14;
+
+console.log((age >= 18) ? "Adult" : "Underage");
+
+
+//-----[EXAMPLE 3]------------------------------------------------------------------------------------------------
+
+var stop; // THIS WILL TAKE A BOOLEAN VALUE
+
+(age >= 18) ? (
+    consol.log("ADULT!!!"),
+    stop = false
+) : (
+    console.log("SORRY, TOO YOUNG"),
+    stop = true
+);
+
+
+//-----[EXAMPLE 4]----------------------------------------------------------------
+
 let mark = 85;
 
+// STORING THE RESULT OF THE TERNARY OPERATOR 
+// IN A VARIABLE
 let result = (mark > 50) ? "Pass" : "Fail"; 
 
-console.log(result)
-
+console.log(result);
 
 
 
--- BASICS/scope.js
+++ BASICS/scope.js
@@ -1,19 +1,42 @@
-//-----[SCOPE IN JS]----------------------------------------------------
+//-----[VARIABLE SCOPE IN JS]----------------------------------------------------
 
 // GLOBAL SCOPE
 
+// - GLOBAL VARIABLES CAN BE ACCESSED FROM ANYWHERE
+
+// IF VAR IS USED TO DEFINE A GLOBAL VARIABLE, 
+// THEN IT WILL APPEAR ON THE WINDOW OBJECT
+
+var a = 20; // APPEARS ON WINDOW OBJECT
+let b = 50; // NOT ON WINDOW OBJECT
+const c = 80; // NOT ON WINDOW OBJECT
+
+
 // FUNCTION SCOPE
 
-// BLOCK SCOPE
+// - VARIABLES DECLARED BY VAR WITHIN A FUNCTION
+// CANNOT BE ACCESSED OUTSIDE OF IT
+
+function dbz() {
+    var z = "goku"; // ONLY APPLIES TO THE "VAR" KEYWORD
+}
+
+console.log(z); // UNDEFINED 
 
 
+//-----[BLOCK SCOPE]---------------------------------------------------------------------
 
+// - LET AND CONST VARIABLES DECLARED WITHIN {} 
+// CANNOT BE ACCESSED OUTSIDE OF IT
 
+if (100 > 20) {
+    let d = 500;
+    const m = 600;
+    var s = 700;
+}
 
-
-
-
-
-
+console.log(d); // UNDEFINED
+console.log(m); // UNDEFINED
+console.log(s); // 700
 
 
